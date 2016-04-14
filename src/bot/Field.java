6a35bfc1f32a36a51eab2c26331b70a9bb2ea151
crisro96 <crisro996@gmail.com>
2016-04-15 01:34:56 +0300

    Update Field.java

--- src/bot/Field.java
+++ src/bot/Field.java
@@ -254,7 +254,7 @@ class Field
         {
             for (int row = 0; row < 3; row++)
                 for (int col = 0; col < 3; col++)
-                    if (mMacroboard[row][col] < 1 && !isFull(mMacroboard[row][col])) // Not won/lost - ties don't matter YET
+                    if (mMacroboard[row][col] < 1 && !isFull(getBoard(row, col))) // Not won/lost - ties don't matter YET
                         mMacroboard[row][col] = -1;
         } else
         {
